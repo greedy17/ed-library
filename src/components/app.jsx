bbea444a3bebb70c87011d95b2f8acb51468c54b
greedy17 <61763082+greedy17@users.noreply.github.com>
2020-12-07 21:44:50 -0500

    Placeholders for current book, previous and next buttons. Also added function to switch between books

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,17 +3,45 @@ import TitleBar from './TitleBar/titleBar';
 import './app.css'
 
 class App extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
+    this.books = [
+      {title: "Ready Player One", author: "Ernest Cline"},
+      {title: "All The Light We Cannot See", author: "Anthony Doer"},
+      {title: "The First and Last Freedom", author: "Jiddu Krishnamurti"}
+    ]
     this.state = {
-
+      bookNumber: 0
     }
+  }
+
+  goToNextBook(){
+    let tempBookNumber = this.state.bookNumber;
+    tempBookNumber++;
+    if(tempBookNumber === this.books.length){
+      tempBookNumber = 0;
+    }
+    this.setState({
+      bookNumber: tempBookNumber
+    })
   }
 
   render(){
     return (
       <div className='container-fluid'>
         <TitleBar/>
+        <div className='row row-spacer'>
+          <div className='col'>
+            {/*prev button*/}
+          </div>
+          <div className='col'>
+             <h1>{this.books[this.state.bookNumber].title}</h1>
+             <h4>{this.books[this.state.bookNumber].author}</h4>
+          </div>
+          <div className='col'>
+             {/*prev button*/}
+          </div>
+        </div>
       </div>
     )
   }
